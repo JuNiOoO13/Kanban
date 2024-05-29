d450e25173c9118464b8ac56d888c84a4f88929e
junior <junioroooppp@gmail.com>
2024-05-29 13:06:02 -0300

    versão_final

--- Kanban-Angular/src/app/app.routes.ts
+++ Kanban-Angular/src/app/app.routes.ts
@@ -1,9 +1,32 @@
-import { Routes } from '@angular/router';
+import { RedirectCommand, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './guards/auth.guard';
+import { BoardListComponent } from './components/board-list/board-list.component';
 
 export const routes: Routes = [
     {
-        path:"",
-        component:HomeComponent
+        path:"register",
+        component:RegisterComponent
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"boards",
+        component:BoardListComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path:"boards/:id",
+        component:HomeComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path:"**",
+        redirectTo:"login"
     }
+   
 ];
